b253a9a2d87fc4def66385f90ea6550ce38c1d82
EdikV <edward.vishnivetzki@gmail.com>
2023-03-04 22:10:47 +0200

    locators

--- Class_Ex/test_classex.py
+++ Class_Ex/test_classex.py
@@ -4,31 +4,36 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 
-@pytest.fixture()
-def browser():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.maximize_window()
-    yield driver
-    driver.quit()
 
-def test_atid_store(browser):
-    browser.get("https://atid.store")
+class Test_ecom:
+    @pytest.fixture()
+    def test_setup(self):
+        global driver
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.maximize_window()
+        yield driver
+        driver.quit()
 
-    store_menu_link = browser.find_element_by_css_selector("li[id='menu-item-45'] a[class='menu-link']")
-    store_menu_link.click()
+    def test_atid_store(self, test_setup):
+        driver.get("https://atid.store")
 
-    sort_by_dropdown = browser.find_element_by_xpath("//select[@name='orderby']")
+        store_menu_link = driver.find_element_by_css_selector("li[id='menu-item-45'] a[class='menu-link']")
+        store_menu_link.click()
 
-    options = sort_by_dropdown.find_elements_by_tag_name("option")
-    fifth_option = options[4]
-    fifth_option.click()
+        sort_by_dropdown = driver.find_element_by_xpath("//select[@name='orderby']")
 
-    time.sleep(5)
+        options = sort_by_dropdown.find_elements_by_tag_name("option")
+        fifth_option = options[4]
+        fifth_option.click()
 
-    search_box = browser.find_element(By.ID, "wc-block-search__input-1").send_keys("Buddha Bracelet")
-    search_box = browser.find_element(By.CLASS_NAME, "wc-block-product-search__button").click()
+        time.sleep(5)
 
-    time.sleep(15)
+        search_box = driver.find_element(By.ID, "wc-block-search__input-1").send_keys("Buddha Bracelet")
+
+
+        search_box = driver.find_element(By.CLASS_NAME, "wc-block-product-search__button").click()
+
+        time.sleep(15)
 
    
 
